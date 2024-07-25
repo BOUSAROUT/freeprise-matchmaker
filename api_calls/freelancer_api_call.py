a085936c2107f2f66f9e830dc91f7c1bd1769da3
Denise Chan <waeyee.chan@gmail.com>
2024-07-25 18:19:28 +0000

    Created data_utils for uploading files into the google bucket. Fixed all bugs in freelancer_api_call

--- api_calls/freelancer_api_call.py
+++ api_calls/freelancer_api_call.py
@@ -11,16 +11,11 @@ import pandas as pd
 from typing import Dict, Union, List
 from pandera.typing import DataFrame
 from data_utils.upload import upload_to_lake, get_data_files
-# from freeprise-matchmaker.utils.upload import upload_to_lake, get_data_files
-
 
 # Load environment variables from .env file
 load_dotenv()
 
-# # Keywords to search the freelancer api with for testing only
-# search_keywords = ['engineer', 'data', 'design', 'web']
-
-# User data columns that are ignored
+# User data columns that are ignored and not uploaded to bucket
 exclude_user_cols = [
     'limited_account',
     'pool_ids',
@@ -38,7 +33,7 @@ exclude_user_cols = [
     'timezone'
 ]
 
-# Portfolio data columns that are ignored
+# Portfolio data columns that are ignored and not uploaded to bucket
 exclude_portfolio_cols = [
     'content_type',
     'featured',
@@ -71,10 +66,11 @@ list_user_cols = [
 # Get a freelancersdk Session object to use the api
 def get_fln_session() -> Session:
     """
-    To do: <describe function>
-    Args: None
+    Creates a freelancer.com session
+    Args:
+        - None
     Returns:
-        Session object
+        - Session object
     """
     url = os.environ.get('FLN_URL')
     oauth_token = os.environ.get('FLN_API_TOKEN')
@@ -83,10 +79,11 @@ def get_fln_session() -> Session:
 
 def get_search_terms(infile: str) -> List[str]:
     """
-    Get search terms for the freelancer api from an input file
-    Args: None
+    Get search terms to call the freelancer api with from an input file
+    Args:
+        - infile: the input file name as a string
     Returns:
-        Session object
+        - A list of keywords
     """
     with open(infile, 'r') as file:
         search_terms = file.read().replace('\n', ' ')
@@ -96,13 +93,12 @@ def get_search_terms(infile: str) -> List[str]:
     return results
 
 
-# Search for freelancer_users
 def get_freelance_users(session: Session, query: Union[str, List[str]]) -> Dict:
     """
-    To do: <describe function>
+    Get the data for users who are freelancers on freelancer.com
     Args:
-        session: The freelancersdk Session object
-        query: The query as a string or a list of strings
+        - session: The freelancersdk Session object
+        - query: The query as a string or a list of strings
     Returns:
         A dictionary with the following keys:
             - users - <to do>
@@ -134,15 +130,16 @@ def get_freelance_users(session: Session, query: Union[str, List[str]]) -> Dict:
         return result
 
 
-# Search for portfolios based on user ds
 def get_user_portfolios(session: Session, user_ids: Union[str, List[str]])  -> Dict:
     """
-    To do: <describe function>
+    Get the data for the portfolios associated with the user based on their user id
     Args:
-        arg_name: <arg_desc>
-        arg_name: <arg_desc>
+        session: The freelancersdk Session object
+        user_ids: The user ids as a string or a list of strings
     Returns:
-        <return description>
+        A dictionary with the following keys:
+            - users - <to do>
+            - total_count - <to do>
     """
     user_ids = user_ids
     try:
@@ -155,15 +152,16 @@ def get_user_portfolios(session: Session, user_ids: Union[str, List[str]])  -> D
         return portfolios
 
 
-# Create user df
 def create_user_df(session: Session, search_keywords: List[str], exclude_cols: List[str], ofile_path: str) -> DataFrame:
     """
-    To do: <describe function>
+    Create a dataframe of user information by calling the freelancer.com api with search keys
     Args:
-        arg_name: <arg_desc>
-        arg_name: <arg_desc>
+        - session: The freelancersdk Session object
+        - search_keywords: A list of keywords as a string or list of strings
+        - exclude_cols: Columns to exclude from the final output
+        - ofile_path: The name of the outfile saved as a parquet file
     Returns:
-        <return description>
+        - Dataframe of user information
     """
     df_list = [pd.DataFrame.from_dict(get_freelance_users(session,keyword)['users']) for keyword in search_keywords]
     # Concatenate list into one consolidated dataframe
@@ -174,28 +172,29 @@ def create_user_df(session: Session, search_keywords: List[str], exclude_cols: L
     print(f"User dataframe shape: {df.shape}")
     # Save as parquet file
     df.to_parquet(ofile_path, engine='pyarrow', index=False)
-    return df # return not used
+    return df
 
 def get_unique_user_ids(df: DataFrame) -> List[int]:
     """
     Get a list of user ids from the user dataframe extracted from the freelancer api
     Args:
-        arg_name: <arg_desc>
-        arg_name: <arg_desc>
+        - df: The user dataframe
     Returns:
-        <return description>
+        - A list of unique user ids
     """
     return df['id'].unique().tolist()
 
 
-def create_portfolio_df(session: Session, user_ids: List[int], exclude_cols: List[str], ofile_path: str) -> DataFrame:
+def create_portfolio_df(session: Session, user_ids: List[int], exclude_cols: List[str], ofile_path: str) -> None:
     """
     Get the portfolio data from a list of user ids using the portfio api and saves the results as parquet
     Args:
-        arg_name: <arg_desc>
-        arg_name: <arg_desc>
+        - session: The freelancersdk Session object
+        - user_ids: A list of unique user ids
+        - exclude_cols: Columns to exclude from the final output
+        - ofile_path: The name of the outfile saved as a parquet file
     Returns:
-        <return description>
+        - None
     """
     # Need to split the user ids into a batch size of 100 because that is the maximum api limit
     batch_size = 100
@@ -228,18 +227,17 @@ def create_portfolio_df(session: Session, user_ids: List[int], exclude_cols: Lis
     # Save as parquet file
     df.to_parquet(ofile_path, engine='pyarrow', index=False)
 
-    return df # return not used
-
 
 # Extract columns so they can be subjected to cleaning and transformation during ETL process - for raw
-def extract_cols_for_cleaning(df: DataFrame, cols: List[str], ofile_path: str) -> DataFrame:
+def extract_cols_for_cleaning(df: DataFrame, cols: List[str], ofile_path: str) -> None:
     """
-    To do: <describe function>
+    Extract columns which need to be unpacked for cleaning
     Args:
-        arg_name: <arg_desc>
-        arg_name: <arg_desc>
+        - df: Input user dataframe
+        - cols: List of columns in the dataframe to be extracted
+        - ofile_path: The name of the out file
     Returns:
-        <return description>
+        - None
     """
     df = df[cols]
     df.to_parquet(ofile_path, engine='pyarrow', index=False)
@@ -248,55 +246,50 @@ def extract_cols_for_cleaning(df: DataFrame, cols: List[str], ofile_path: str) -
     return df # return not used
 
 
-# Perform light cleaning on user df - for silver
-def user_df_light_cleanse(df: DataFrame, cols: List[str], ofile_path: str) -> DataFrame:
+# Perform light cleaning on user df - for raw
+def user_df_light_cleanse(df: DataFrame, cols: List[str], ofile_path: str) -> None:
     """
-    To do: <describe function>
+    Performances deduplication on the user dataframe
     Args:
-        arg_name: <arg_desc>
-        arg_name: <arg_desc>
+        - df: Input user dataframe
+        - cols: List of columns in the dataframe to be extracted
+        - ofile_path: The name of the out file
     Returns:
-        <return description>
+        - None
     """
     _df = user_df.drop(cols, axis=1)
     df = _df.drop_duplicates(ignore_index=True)
     df.to_parquet(ofile_path, engine='pyarrow', index=False)
     print(f"{_df.shape[0] - df.shape[0]} duplicate rows dropped")
     print(f"User dataframe shape: {df.shape}")
-    return df # return not used
+
 
 
 
 if __name__ == '__main__':
-    # # The following steps describe the data extraction process for the freelancer.com user and portfolio data
-    # session = get_fln_session()
-    # search_terms = get_search_terms('freelancer_unique_search_terms.txt')
-    # # For testing, search using the first 20 keywords only
-    # test_keywords = search_terms[0:20]
-    # # Create the user dataframe
-    # user_df = create_user_df(session, test_keywords, exclude_user_cols, 'data/raw/fln_users_all.parquet')
-    # # Get the list of unique users
-    # user_ids = get_unique_user_ids(user_df)
-    # # Extract user dict columns that require ETL to cleanse (e.g. deduplication)
-    # dict_user_df = extract_cols_for_cleaning(user_df, dict_user_cols, 'data/raw/fln_users_dict_cols.parquet')
-    # # Extract user list of dict columns that require ETL to cleanse (e.g. deduplication)
-    # list_user_df = extract_cols_for_cleaning(user_df, list_user_cols, 'data/raw/fln_users_list_dict_cols.parquet')
-    # # Extract other user columns that can be deduplicated
-    # cleanse_cols_exc = dict_user_cols + list_user_cols
-    # # There's a bug here [Errno 2] No such file or directory: 'data/silver/fln_users.parquet': check for dir before proceeding
-    # user_clean_df = user_df_light_cleanse(user_df,cleanse_cols_exc, 'data/silver/fln_users.parquet')
-    # # create the portfolio dataframe
-    # portfolio_df = create_portfolio_df(session, user_ids, exclude_portfolio_cols, 'data/raw/fln_portfolio.parquet')
+    # The following steps describe the data extraction process for the freelancer.com user and portfolio data
+    # This is for testing locally and the script below needs to be transformed to Airflow to perform these actions as a dag
+    session = get_fln_session()
+    search_terms = get_search_terms('freelancer_unique_search_terms.txt')
+    # For testing, search using the first 20 keywords only
+    test_keywords = search_terms[0:20]
+    # Create the user dataframe
+    user_df = create_user_df(session, test_keywords, exclude_user_cols, 'data/raw/fln_users_all.parquet')
+    # Get the list of unique users
+    user_ids = get_unique_user_ids(user_df)
+    # Extract user dict columns that require ETL to cleanse (e.g. deduplication)
+    dict_user_df = extract_cols_for_cleaning(user_df, dict_user_cols, 'data/raw/fln_users_dict_cols.parquet')
+    # Extract user list of dict columns that require ETL to cleanse (e.g. deduplication)
+    list_user_df = extract_cols_for_cleaning(user_df, list_user_cols, 'data/raw/fln_users_list_dict_cols.parquet')
+    # Extract other user columns that can be deduplicated
+    cleanse_cols_exc = dict_user_cols + list_user_cols
+    # There's a bug here [Errno 2] No such file or directory: 'data/silver/fln_users.parquet': check for dir before proceeding
+    user_clean_df = user_df_light_cleanse(user_df,cleanse_cols_exc, 'data/raw/fln_users.parquet')
+    # create the portfolio dataframe
+    portfolio_df = create_portfolio_df(session, user_ids, exclude_portfolio_cols, 'data/raw/fln_portfolio.parquet')
 
     # upload files to raw dir in bucket
     raw_files = get_data_files()
 
     for file in raw_files:
-        # Need to fix bug [Errno 2] No such file or directory: 'fln_users_all.parquet' -
-        upload_to_lake(file)
-
-    # # upload files to silver dir in bucket
-    # silver_files = get_data_files(data_dir='silver')
-
-    # for file in raw_files:
-    #     upload_to_lake('silver', file)
+        upload_to_lake('freelancer.com', file)
--- data_utils/upload.py
+++ data_utils/upload.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from google.cloud import storage
 from typing import List
+from dotenv import load_dotenv
 import os
 
 def get_data_files(data_dir: str = 'raw') -> List[str]:
@@ -18,30 +19,32 @@ def get_data_files(data_dir: str = 'raw') -> List[str]:
     current_dir = os.getcwd()
     folder = os.path.join(current_dir, 'data', data_dir)
     try:
+        if not os.path.exists(folder):
+            os.makedirs(folder)
         return os.listdir(folder)
     except Exception as e:
         print('Error message: {}'.format(e))
         return None
 
 
-def upload_to_lake(file_name: str, bucket_dir: str = 'raw') -> None:
+def upload_to_lake(datasource: str, file_name: str) -> None:
     """
     Uploads a file to a GCP bucket, organizing it in a data lake's raw zone with a date-based structure.
 
     Args:
-    - bucket_dir: raw or silver
+    - datasource: The source of the data e.g. freelancer.com, kaggle
     - file_name (str): The name of the file to upload.
 
     Returns:
-    None
+    - None
     """
     date_today = datetime.now()
     year = date_today.strftime("%Y")
     month = date_today.strftime("%m")
     day = date_today.strftime("%d")
-    ifile_path = f"data/{bucket_dir}/{file_name}"
+    ifile_path = f"data/raw/{file_name}"
 
-    blob_path = f"{bucket_dir}/freelancer/{year}/{month}/{day}/{file_name}"
+    blob_path = f"{datasource}/{year}_{month}_{day}/{file_name}"
 
     storage_client = storage.Client()
 
@@ -49,6 +52,17 @@ def upload_to_lake(file_name: str, bucket_dir: str = 'raw') -> None:
     bucket = storage_client.bucket(bucket_name)
 
     blob = bucket.blob(blob_path)
-    # This need to take the full path
-    blob.upload_from_filename(ifile_path)
-    print(f"File {file_name} uploaded to {blob_path}.")
+
+    try:
+        blob.upload_from_filename(ifile_path)
+        print(f"File {file_name} uploaded to {blob_path}.")
+    except Exception as e:
+        print('Error message: {}'.format(e))
+
+
+
+if __name__ == '__main__':
+    # Load environment variables from .env file
+    load_dotenv()
+    print(get_data_files())
+    upload_to_lake('test', 'test.txt')
